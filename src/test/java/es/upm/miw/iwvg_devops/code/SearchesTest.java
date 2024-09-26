e88a1ee77a1547a25041c96470313b65e7a1c9f9
BrayanVera <brayanpablo@outlook.com>
2024-09-26 03:11:37 +0200

    added search 5 and Test search 5 #13

--- src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
+++ src/test/java/es/upm/miw/iwvg_devops/code/SearchesTest.java
@@ -20,4 +20,13 @@ public class SearchesTest {
 
     }
 
+    @Test
+    void TestFindUserIdByAllProperFraction(){
+
+        Stream<String> result = new Searches().findUserIdByAllProperFraction();
+        List<String> expectedResult = Arrays.asList("1", "2", "3", "5");
+        assertEquals(expectedResult, result.collect(Collectors.toList()));
+
+    }
+
 }
